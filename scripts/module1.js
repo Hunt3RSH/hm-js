78ee7fb96a3270cb27d2d967dd68b260f177a8c0
Volodymyr Shamrai <s.v.777.energy@gmail.com>
2022-06-22 16:58:23 +0300

    js start

---
+++ scripts/module1.js
@@ -0,0 +1,84 @@
+// Задача 5 ///////////////////////////////////////////////////////
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// Задача 6 ///////////////////////////////////////////////////////
+
+// const orderedQuantity = 6;
+// const pricePerDroid = 800;
+// const deliveryFee = 50;
+// const totalPrice = ((pricePerDroid * orderedQuantity) + deliveryFee);
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// console.log(totalPrice)
+// console.log(message)
+
+// Задача 7 ///////////////////////////////////////////////////////
+
+// function sayHi() {
+//     console.log('Hello, this is my first function!')
+// }
+// sayHi()
+
+// Задача 8 ///////////////////////////////////////////////////////
+
+// function add(a, b, c) {
+//     console.log(`Addition result equals ${a + b + c}`);
+//   }
+
+//   add(15, 27, 10);
+//   add(10, 20, 30);
+//   add(5, 10, 15);
+
+// Задача 9 ///////////////////////////////////////////////////////
+
+// function add(a, b, c) {
+//     return a + b + c
+//   }
+
+//   let result = add(2, 5, 8);
+//   console.log(result)
+
+//   console.log(add(15, 27, 10));
+//   console.log(add(10, 20, 30));
+//   console.log(add(5, 10, 15));
+
+// Задача 10 ///////////////////////////////////////////////////////
+
+// function makeMessage (name, price) {
+//      const message = `You picked ${name}, price per item is ${price} credits`;
+//     return message;
+//   };
+
+//   console.log(makeMessage(`Radar`, 6150));
+//   console.log(makeMessage('Scanner', 3500));
+//   console.log(makeMessage(`Reactor`, 8000));
+//   console.log(makeMessage(`Engine`, 4070));
+
+// Задача 11 ///////////////////////////////////////////////////////
+
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//     const totalPrice = (orderedQuantity * pricePerItem);
+
+//     return totalPrice;
+//   };
+//   console.log(calculateTotalPrice(5, 100));
+//   console.log(calculateTotalPrice(8, 60));
+//   console.log(calculateTotalPrice(3, 400));
+//   console.log(calculateTotalPrice(1, 3500));
+//   console.log(calculateTotalPrice(12, 70));
+
+// Задача 12 ///////////////////////////////////////////////////////
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//     const message = `You ordered droids worth ${((orderedQuantity*pricePerDroid)+deliveryFee)} credits. Delivery (${deliveryFee} credits) is included in total price.`
+//     return message;
+//   }
+//     console.log(makeOrderMessage(2, 100, 50));
+//     console.log(makeOrderMessage(4, 300, 100));
+//     console.log(makeOrderMessage(10, 70, 200));
+
+// Задача 13 ///////////////////////////////////////////////////////
