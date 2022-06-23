6e5897d17ed3c8bbdaea446bb0df13425a34217f
Volodymyr Shamrai <s.v.777.energy@gmail.com>
2022-06-23 13:07:19 +0300

    hm 27

--- scripts/module1.js
+++ scripts/module1.js
@@ -104,19 +104,17 @@
 //   console.log(isValidPassword("kiwirul3z"))
 //   console.log(isValidPassword("jqueryismyjam"))
 
-
-  // Задача 15 ///////////////////////////////////////////////////////
-
+// Задача 15 ///////////////////////////////////////////////////////
 
 //   function checkAge(age) {
 //     let message;
-  
+
 //     if (age >= 18) {
 //       message = 'You are an adult';
 //     } else {
 //       message = 'You are a minor';
 //     }
-  
+
 //     return message;
 //   }
 //   console.log(checkAge(20))
@@ -124,4 +122,183 @@
 //   console.log(checkAge(14))
 //   console.log(checkAge(38))
 
-  // Задача 16 ///////////////////////////////////////////////////////
+// Задача 16 ///////////////////////////////////////////////////////
+//   function checkStorage(available, ordered) {
+//     let message;
+//     if (available < ordered){
+//         message = `Not enough goods in stock!`
+//     } else {
+//         message = `Order is processed, our manager will contact you.`
+//     }
+//     return message;
+//   }
+
+//     console.log(checkStorage(100, 50))
+//     console.log(checkStorage(100, 130))
+//     console.log(checkStorage(200, 20))
+//     console.log(checkStorage(200, 150))
+//     console.log(checkStorage(150, 180))
+
+// Задача 18 ///////////////////////////////////////////////////////
+
+//   function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//     let message;
+//     let totalPrice;
+//     totalPrice = (pricePerDroid * orderedQuantity)
+//     let balance
+//     balance = (customerCredits-totalPrice)
+//     if (totalPrice <= customerCredits) {
+//         message = `You ordered ${orderedQuantity} droids, you have ${balance} credits left`;
+//     } else {
+//         message = `Insufficient funds!`;
+//     }
+
+//     return message;
+//   }
+
+//   console.log((makeTransaction(3000, 5, 23000)))
+//   console.log((makeTransaction(1000, 3, 15000)))
+//   console.log((makeTransaction(5000, 10, 8000)))
+//   console.log((makeTransaction(2000, 8, 10000)))
+//   console.log((makeTransaction(500, 10, 5000)))
+
+// Задача 19 ///////////////////////////////////////////////////////
+
+//   function checkPassword(password) {
+//     const ADMIN_PASSWORD = 'jqueryismyjam';
+//     let message;
+
+//     if (password === null) {
+//       message =  'Canceled by user!';
+//     } else if (password === ADMIN_PASSWORD) {
+//       message = 'Welcome!';
+//     } else {
+//       message = 'Access denied, wrong password!';
+//     }
+
+//     return message;
+//   }
+
+// Задача 20 ///////////////////////////////////////////////////////
+
+// function checkStorage(available, ordered) {
+//     let message;
+//         if (ordered === 0) {
+//   message =  'There are no products in the order!';
+// } else if (ordered > available) {
+//   message = 'Your order is too large, there are not enough items in stock!';
+// } else {
+//   message = 'The order is accepted, our manager will contact you';
+// }
+//     return message;
+//   }
+
+// Задача 21 //////////////////////////////////////////////////////
+
+// function isNumberInRange(start, end, number) {
+// 	const isInRange = number > start && number < end;
+
+// 	return isInRange;
+// }
+// console.log(isNumberInRange(10, 30, 17));
+// console.log(isNumberInRange(10, 30, 5));
+
+// Задача 22 //////////////////////////////////////////////////////
+
+// function checkIfCanAccessContent(subType) {
+//   const canAccessContent = subType === "pro" || subType === "vip" ;
+
+//   return canAccessContent;
+// }
+
+// console.log(checkIfCanAccessContent("pro"));
+// console.log(checkIfCanAccessContent("starter"));
+// console.log(checkIfCanAccessContent("vip"));
+// console.log(checkIfCanAccessContent("free"));
+
+// Задача 23 //////////////////////////////////////////////////////
+
+// function isNumberNotInRange(start, end, number) {
+//     const isInRange = number >= start && number <= end;
+//     const isNotInRange = !isInRange;
+
+//     return isNotInRange;
+// }
+
+// console.log(isNumberNotInRange(10, 30, 17))
+// console.log(isNumberNotInRange(10, 30, 5))
+
+// Задача 24 ////////////////////////////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// function getDiscount(totalSpent) {
+// 	const BASE_DISCOUNT = 0;
+// 	const BRONZE_DISCOUNT = 0.02;
+// 	const SILVER_DISCOUNT = 0.05;
+// 	const GOLD_DISCOUNT = 0.1;
+//     let discount;
+//     if (totalSpent < 5000) {
+//         discount = BASE_DISCOUNT
+//     } else if (totalSpent >= 5000 && 20000 > totalSpent) {
+//         discount = BRONZE_DISCOUNT
+//     } else if (totalSpent >= 20000 && 50000 > totalSpent) {
+//         discount = SILVER_DISCOUNT
+//     } else if (totalSpent >= 50000) {
+//         discount = GOLD_DISCOUNT
+//     }
+// 	return discount;
+// }
+
+// console.log(getDiscount(137000));
+// console.log(getDiscount(46900));
+// console.log(getDiscount(8250));
+// console.log(getDiscount(1300));
+// console.log(getDiscount(5000));
+
+// Задача 25 //////////////////////////////////////////////////////
+
+// function checkStorage(available, ordered) {
+//     let message;
+//     message = ordered > available ? "Not enough goods in stock!" : "The order is accepted, our manager will contact you";
+
+//     return message;
+// }
+// console.log(checkStorage(100, 100));
+
+// Задача 26 //////////////////////////////////////////////////////
+
+// function checkPassword(password) {
+// 	const ADMIN_PASSWORD = "jqueryismyjam";
+//     let message;
+//     message = password === ADMIN_PASSWORD ? "Access is allowed" : "Access denied, wrong password!";
+// 	return message;
+// }
+// console.log(checkPassword("jqueryismyjam"));
+// console.log(checkPassword("angul4r1sl1f3"));
+
+// Задача 27 ////////////////////////////////////////////////////// 222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+
+function getSubscriptionPrice(type) {
+  let price;
+  // Change code below this line
+
+ switch (tyoe) { // Change this line
+    case : // Change this line
+      price = 0; // Change this line
+      break;
+
+    case : // Change this line
+      price = 20; // Change this line
+      break;
+
+    case : // Change this line
+      price = 50; // Change this line
+      break;
+  }
+
+  // Change code above this line
+  return price;
+}
+
+console.log(getSubscriptionPrice("organization"));
+console.log(getSubscriptionPrice("starter"));
+console.log(getSubscriptionPrice("professional"))
